041f95cbcc258a014aa8e0159ba1d651b90d5575
Helmut Hoffer von Ankershoffen <hhva@20steps.de>
2017-08-15 14:50:25 +0200

    jwt

--- src/Bricks/Custom/Twentysteps/AlexaBrick/AlexaBundle/OAuth/JWTBearerExtension.php
+++ src/Bricks/Custom/Twentysteps/AlexaBrick/AlexaBundle/OAuth/JWTBearerExtension.php
@@ -2,6 +2,7 @@
 	
 	namespace Bricks\Custom\Twentysteps\AlexaBrick\AlexaBundle\OAuth;
 	
+	use Bricks\Custom\Twentysteps\AlexaBrick\AlexaBundle\Entity\User;
 	use Symfony\Component\DependencyInjection\Container;
 	use Monolog\Logger;
 	
@@ -10,12 +11,13 @@
 	use OAuth2\OAuth2;
 
 	use FOS\OAuthServerBundle\Storage\GrantExtensionInterface;
-
+	use FOS\UserBundle\Doctrine\UserManager;
+	
 	use twentysteps\Commons\EnsureBundle\Ensure;
 	
 	/**
-	 * Class SocialGrantExtension
-	 * @package AppBundle\OAuth
+	 * Class JWTBearerExtension
+	 * Cp. https://developers.google.com/actions/identity/oauth2-assertion-flow
 	 */
 	class JWTBearerExtension implements GrantExtensionInterface {
 		
@@ -52,17 +54,112 @@
 			
 			$this->logger->debug(var_export($jwt,true));
 			
+			// alg: RS256, kid: ca04df587b5a7cead80abee9ea8dcf7586a78e01
+			
+			$userId = $jwt->claims['sub'];
+			$userEmail = $jwt->claims['email'];
+			$userEmailVerified = $jwt->claims['email_verified'];
+			$userFirstName = $jwt->claims['given_name'];
+			$userLastName = $jwt->claims['family_name'];
+			$userLocale = $jwt->claims['locale'];
+			
+			
+			$userManager = $this->getUserManager();
 			switch($intent) {
 				case 'get':
-					break;
+					/**
+					 * @var User $user
+					 */
+					$user = $userManager->findUserBy(array('googleId' => $userId));
+					if (!$user) {
+						$user = $userManager->findUserByEmail($userEmail);
+					}
+					if (!$user) {
+						throw new OAuth2ServerException(OAuth2::HTTP_UNAUTHORIZED, 'user_not_found', "User was not found");
+					}
+
+					// possibly set user's Google Id and enable user
+					$user->setGoogleId($userId);
+					if (!$user->isEnabled()) {
+						// change password for security reason if user is not enabled
+						// i.e. has not confirmed the form based registration
+						$user->setPassword($this->generateRandomPassword());
+						$user->setEnabled(true);
+					}
+
+					// enhance profile
+					if (!$user->getFirstName() || $user->getFirstName()=='') {
+						$user->setFirstName($userFirstName);
+					}
+					
+					$userManager->updateUser($user);
+					
+					// return user reference so token is assigned to user
+					return ['data' => $user];
+					
 				case 'create':
-					break;
+					/**
+					 * @var User $user
+					 */
+					$user = $userManager->findUserBy(array('googleId' => $userId));
+					if (!$user) {
+						$user = $userManager->findUserByEmail($userEmail);
+					}
+					if ($user) {
+						throw new OAuth2ServerException(OAuth2::HTTP_UNAUTHORIZED, 'linking_error', "User already exists",$user->getEmailCanonical());
+					}
+					
+					// user is new, create and connect it
+					$user = $userManager->createUser();
+					$user->setUsername($this->generateRandomUsername($userId, 'google'));
+					$user->setEmail($userEmail);
+					$user->setPassword($this->generateRandomPassword());
+					$user->setEnabled(true);
+					
+					$user->setRegistrationType('oauth:google');
+					$user->setGoogleId($userId);
+					
+					$user->setFirstName($userFirstName);
+					
+					$userManager->updateUser($user);
+
+					// return user reference so token is assigned to user
+					return ['data' => $user];
+
 				default:
 					Ensure::fail('intent not supported');
 			}
 			
-			throw new OAuth2ServerException(OAuth2::HTTP_FORBIDDEN, 'bla', "The redirect URI is missing or do not match");
-			return null;
+			Ensure::fail('coding error ');
+		}
+		
+		// helpers
+		
+		/**
+		 * @return UserManager
+		 */
+		protected function getUserManager() {
+			return $this->sc->get('bricks.custom.twentysteps_alexa.user_manager');
+		}
+		
+		/**
+		 * Generates a random username with the given
+		 * e.g 12345_github, 12345_facebook
+		 *
+		 * @param string $username
+		 * @param string $serviceName
+		 * @return string
+		 */
+		private function generateRandomUsername($username, $serviceName){
+			if(!$username){
+				$username = "user". uniqid(rand(), true) . $serviceName;
+			}
+			
+			return $username. "_" . $serviceName;
+		}
+		
+		private function generateRandomPassword() {
+			return uniqid(rand(), true);
 		}
 		
 	}
